425580ad0931e7d310b825ab2716031701ff1e9d
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2019-11-23 21:53:24 +0000

    Process and add some limited food data

---
+++ src/data/initialData.js
@@ -0,0 +1,65 @@
+import rawData from './almostRawInitialData';
+
+const initialStructuredData = rawData
+    .map(reshapeIntoExpectedStructure)
+    .map(ensureIdIsANonZeroInteger)
+    .map(integeriseTraceAmountsInMacros);
+
+function reshapeIntoExpectedStructure(item) {
+    // some simple hash implementation from StackOverflow
+    const hashCode = argString => {
+        let hash = 0;
+        for (let i = 0; i < argString.length; i++) {
+            let char = argString.charCodeAt(i);
+            hash = ((hash<<5)-hash)+char;
+            hash = hash & hash; // Convert to 32bit integer
+        }
+        return hash;
+    };
+
+    return {
+        version: 1,  // to enable immutability but still allow changes
+
+        id: hashCode(JSON.stringify(item)),
+        name: item.name,
+        macros: {  // quantities per 100g of product
+            fat: item.fat,
+            protein: item.protein,
+            carbs: item.carbs,
+        },
+        extra: item.extra,
+
+        unit: "g",  // for quantity display only
+        portionSize: 1,  // grams per portion
+        portions: 1,
+        ingredients: [],
+        usedBy: [],  // list of <id, version> of depending meals
+
+        // to be used, maybe, in the future
+        // portionStep: 5,  // in grams
+    };
+}
+
+function ensureIdIsANonZeroInteger(item) {
+    if (item.id === 0) throw new Error(`Invalid id (expected non-zero) on item "${item.name}"; the value is "${item.id}"`);
+    if (Number.isInteger(item.id)) return item;
+    throw new Error(`Invalid value (expected integer) on item "${item.name}"; the value is "${item.id}"`);
+}
+
+function integeriseTraceAmountsInMacros(item) {
+    const treatTraceAmountAsZero = (value, field) => {
+        if (value === "Tr") return 0;
+        value = Number.parseInt(value);
+        if (Number.isInteger(value)) return value;
+        if (value === 0.0) return 0;
+        throw new Error(`Invalid value (expected int or "Tr") on item "${item.name}", field '${field}'; the value is "${value}"`);
+    };
+
+    item.macros.fat = treatTraceAmountAsZero(item.macros.fat, "fat");
+    item.macros.protein = treatTraceAmountAsZero(item.macros.protein, "protein");
+    item.macros.carbs = treatTraceAmountAsZero(item.macros.carbs, "carbs");
+
+    return item;
+}
+
+export default initialStructuredData;
