3ff9e8e1c2d76e0a09c966debbb8161e9034d182
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2019-12-22 15:22:51 +0000

    Add 'findFood' store accessor

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -12,4 +12,24 @@ function createEmptyStore() {
     return createStore(Reducer, initialStateShape);
 }
 
-export { createEmptyStore };
+const LATEST = Symbol("latest version");
+function findFood(state, foodId, foodVersion = LATEST) {
+    if (foodVersion === LATEST) {
+        // TODO
+        throw new Error("'findFood' at version LATEST is not implemented yet");
+    }
+
+    const allVersionsOfSelectedFood = state.current.foodData
+            .filter(food => food.id === foodId);
+    
+    const theChosenOne = allVersionsOfSelectedFood.filter(food => food.version === foodVersion);
+    if (theChosenOne.length !== 1) {
+        console.error(`Expected exactly one food with id ${foodId}` +
+            ` and version ${foodVersion} but found ${theChosenOne.length}`);
+    }
+
+    return theChosenOne[0];
+}
+
+
+export { createEmptyStore, findFood };
