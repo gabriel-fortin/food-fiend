0746997fca35c9d690e9e02104c296c0fc175685
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2020-01-05 20:16:10 +0000

    Add input type-checking to 'changeIngredientQuantity'

--- src/ReduxyStuff/ActionCreators.js
+++ src/ReduxyStuff/ActionCreators.js
@@ -1,6 +1,20 @@
-export { importData, changeIngredientQuantity }
+import PropTypes from 'prop-types';
+
+/*
+ * This file is an API to the store.
+ * All inputs should be type-verified here before actions are dispatched.
+ */
+
+const IngredientQuantityChangeAction_PropTypeDef = {
+    mealId: PropTypes.number.isRequired,
+    mealVersion: PropTypes.number.isRequired,
+    ingredientPosInMeal: PropTypes.number.isRequired,
+    newQuantity: PropTypes.number.isRequired,
+};
+
 
 function importData(data) {
+    // TODO: ActionCreators: validate input for 'importData'
     return {
         type: "IMPORT_DATA",
         data,
@@ -8,11 +22,18 @@ function importData(data) {
 }
 
 function changeIngredientQuantity(mealId, mealVersion, ingredientPosInMeal, newQuantity) {
-    return {
+    const action = {
         type: "CHANGE_FOOD_QUANTITY",
         mealId,
         mealVersion,
         ingredientPosInMeal,
         newQuantity,
     };
+
+    PropTypes.checkPropTypes(IngredientQuantityChangeAction_PropTypeDef, action,
+        `parameter`, `${changeIngredientQuantity.name}`);
+
+    return action;
 }
+
+export { importData, changeIngredientQuantity };
