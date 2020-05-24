9fcc8a17bf5e62c1ba8336e9d53cf3c1f686f42b
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2020-05-24 09:54:41 +0100

    Move FoodAdder to own file

---
+++ src/Widget/Meal/FoodAdder.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Box, Button, Collapse, Flex, IconButton } from "@chakra-ui/core";
+
+import { Onion } from "Onion";
+import { ConnectedFoodSelectorWidget } from "Widget";
+
+
+interface Props {
+    context: Onion,
+}
+
+export const FoodAdder: React.FC<Props> = ({ context }) => {
+    const [showFoodSelector, setShowFoodSelector] = useState(false);
+
+    return (
+        <Box marginX={2} marginY={3}>
+            <Collapse isOpen={!showFoodSelector}>
+                <Button
+                    size="sm"
+                    variant="outline"
+                    variantColor="pink"
+                    borderStyle="solid 1px black"
+                    onClick={() => setShowFoodSelector(true)}
+                >
+                    + Add food
+                </Button>
+            </Collapse>
+            <Collapse isOpen={showFoodSelector}>
+                <Flex alignItems="center">
+                    <IconButton aria-label="close food selector"
+                        variant="outline"
+                        icon="close"
+                        variantColor="red"
+                        size="sm"
+                        mr={2}
+                        opacity={0.6}
+                        _hover={{opacity: 1}}
+                        onClick={() => setShowFoodSelector(false)}
+                    />
+                    <ConnectedFoodSelectorWidget context={context} />
+                </Flex>
+            </Collapse>
+        </Box>
+    );
+};
--- src/Widget/Meal/MealWidget.tsx
+++ src/Widget/Meal/MealWidget.tsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import { Box, Button, Collapse, Flex, IconButton, } from '@chakra-ui/core';
+import React from "react";
 
-import { MacrosInfo } from 'Widget';
-import { Macros, Food, Ingredient } from 'Model';
-import { IngredientsListWidget } from 'Widget';
-import Onion from 'Onion';
-import { ConnectedFoodSelectorWidget as FoodSelectorWidget } from 'Widget';
+import { MacrosInfo } from "Widget";
+import { Macros, Food, Ingredient } from "Model";
+import { IngredientsListWidget } from "Widget";
+import Onion from "Onion";
 
-import './meal-widget.css';
+import { FoodAdder } from "./FoodAdder";
+import "./meal-widget.css";
 
 
-interface MWProps {
+interface Props {
     name: string;
     totalMacros: Macros;
     data: {ingredient: Ingredient, food: Food}[];
     changeIngredientQuantity: (pos: number, q: string) => void;
     uiEnclosure: Onion;
 }
-export const MealWidget: React.FC<MWProps> = ({name, totalMacros, data, changeIngredientQuantity, uiEnclosure = Onion.create()}) => {
+
+export const MealWidget: React.FC<Props> = ({name, totalMacros, data, changeIngredientQuantity, uiEnclosure = Onion.create()}) => {
     // TODO: when IngredientsList becomes more independent, pass 'uiEnclosure' to it
     return (
         <div className="meal">
@@ -34,46 +34,7 @@ export const MealWidget: React.FC<MWProps> = ({name, totalMacros, data, changeIn
                 onQuantityChange={(pos: number, q: string) => changeIngredientQuantity(pos, q)}
                 onSelectionToggle={undefined}
             />
-            <AddFoodWidget context={uiEnclosure} />
+            <FoodAdder context={uiEnclosure} />
         </div>
     );
 }
-
-
-interface AddFoodButtonProps {
-    context: Onion,
-}
-const AddFoodWidget: React.FC<AddFoodButtonProps> = ({ context }) => {
-    const [showFoodSelector, setShowFoodSelector] = useState(false);
-
-    return (
-        <Box marginX={2} marginY={3}>
-            <Collapse isOpen={!showFoodSelector}>
-                <Button
-                    size="sm"
-                    variant="outline"
-                    variantColor="pink"
-                    borderStyle="solid 1px black"
-                    onClick={() => setShowFoodSelector(true)}
-                >
-                    + Add food
-                </Button>
-            </Collapse>
-            <Collapse isOpen={showFoodSelector}>
-                <Flex alignItems="center">
-                    <IconButton aria-label="close food selector"
-                        variant="outline"
-                        icon="close"
-                        variantColor="red"
-                        size="sm"
-                        mr={2}
-                        opacity={0.6}
-                        _hover={{opacity: 1}}
-                        onClick={() => setShowFoodSelector(false)}
-                    />
-                    <FoodSelectorWidget context={context} />
-                </Flex>
-            </Collapse>
-        </Box>
-    );
-};
