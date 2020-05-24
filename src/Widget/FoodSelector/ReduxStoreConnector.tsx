3bf00c27e0d4eb8801fb5d17d77814f8845cb4da
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2020-05-24 14:11:16 +0100

    Add (implicit onion) connector for Food Selector

---
+++ src/Widget/FoodSelector/ReduxStoreConnector.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { State, appendIngredient } from "Store";
+import { useOnion } from "Onion";
+import { Ref } from "Model";
+
+import { FoodSelectorWidget } from "./FoodSelectorWidget";
+
+
+export const ReduxStoreConnector: React.FC<{}> = () => {
+    const onion = useOnion();
+
+    const mapState = (state: State) => ({
+        data: state.getAllLatestFoods(),
+    });
+    
+    const mapDispatch = {
+        onFoodSelected: (ref: Ref) => {
+            // add ingredient to whatever is indicated by the current Onion context
+            return appendIngredient(ref, onion);
+        },
+    };
+
+    const Connected = connect(mapState, mapDispatch)(FoodSelectorWidget);
+    return <Connected />;
+};
