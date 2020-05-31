f094780ca8abcff6a1a3a929aefa8173eb6ca263
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2020-05-31 08:59:49 +0100

    Use chakra for styling ingredient rows

--- src/Widget/IngredientEntry/StyledDataRow.tsx
+++ src/Widget/IngredientEntry/StyledDataRow.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { Box, Flex, PseudoBox } from "@chakra-ui/core";
 
 import { Macros } from "Model";
 import { MacrosInfo } from "Widget";
 import { QuantityEditor } from "UI";
+
+
+// FIX: style of a row is tied to the style of the header (which is defined in IngredientsList)
 
 
 interface Props {
@@ -34,26 +38,49 @@ export const StyledDataRow: React.FC<Props> = ({
         <>
             <Box className="divider" />
 
-            <Box className="name">
-                <span>{name}</span>
-            </Box>
-            <Box className="macros">
-                <MacrosInfo macros={macros} />
-            </Box>
-            <Box className="quantity-editor" onClick={userClicksQuantityValue}>
-                {
-                    /* eslint-disable no-mixed-operators */
-                    editMode
-                    && <QuantityEditor
-                        quantity={quantity}
-                        userAbandonsEditing={userAbandonsEditing}
-                        userAcceptsQuantityChange={userAcceptsQuantityChange}
-                    />
-                    || quantity
-                    /* eslint-enable no-mixed-operators */
-                }
-                <span>g</span>
-            </Box>
+            <PseudoBox
+                gridColumn="1 / -1"
+                width="100%"
+                _hover={{ boxShadow: "1px 1px 13px 1px grey" }}
+            >
+                <Flex
+                    justify="space-between"
+                >
+                    <Box
+                        className="name"
+                        flexBasis="40%"
+                        flexGrow={5}
+                        alignSelf="center"
+                    >
+                        {name}
+                    </Box>
+                    <Box
+                        flexBasis="9em"
+                        flexGrow={1}
+                    >
+                        <MacrosInfo macros={macros} />
+                    </Box>
+                    <Box
+                        flexBasis="5em"
+                        textAlign="center"
+                        onClick={userClicksQuantityValue}
+                        alignSelf="center"
+                    >
+                        {
+                            /* eslint-disable no-mixed-operators */
+                            editMode
+                            && <QuantityEditor
+                                quantity={quantity}
+                                userAbandonsEditing={userAbandonsEditing}
+                                userAcceptsQuantityChange={userAcceptsQuantityChange}
+                            />
+                            || quantity
+                            /* eslint-enable no-mixed-operators */
+                        }
+                        <span>g</span>
+                    </Box>
+                </Flex>
+            </PseudoBox>
         </>
     );
 };
