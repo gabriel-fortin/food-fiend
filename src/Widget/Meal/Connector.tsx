c5479f5acff014d4ae60de152fb8525ca75b4075
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2020-07-06 15:04:26 +0100

    Add meal create-add button to Meal

--- src/Widget/Meal/Connector.tsx
+++ src/Widget/Meal/Connector.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { connect } from "react-redux";
+import { connect, useDispatch } from "react-redux";
 
-import { State, changeFoodName, setErrorMessage } from "Store";
+import { State, changeFoodName, removeIngredient, setErrorMessage } from "Store";
 import { Ref } from "Model";
 import { FoodLayerProvider, useOnion } from "Onion";
 import { IngredientsList } from "Widget";
 import { FoodAdder } from "./FoodAdder";
 
 import { InitiallyStyledMeal as MealUI } from "./InitiallyStyledMeal";
+import { Button, Stack, Modal, useDisclosure, ModalContent, ModalOverlay, ModalBody, ModalCloseButton } from "@chakra-ui/core";
 
 
 interface Props {
@@ -17,16 +18,39 @@ interface Props {
 /**
  * Connects Meal to the redux store
  */
-export const Connector: React.FC<Props> = ({ mealRef }) =>
-    <FoodLayerProvider food={mealRef}>
-        <ConnectedUI mealRef={mealRef}>
-            <IngredientsList
-                mealRef={mealRef}
-            />
-            <FoodAdder />
-        </ConnectedUI>
-    </FoodLayerProvider>
-    ;
+export const Connector: React.FC<Props> = ({ mealRef }) => {
+    const onion = useOnion();
+    
+    const dispatch = useDispatch();
+    const removeMeal = () => {
+        console.log(`Onion: ${JSON.stringify((onion as any).layers, null, 2)}`);
+        dispatch(removeIngredient(onion));
+    };
+
+    // const onCreateMeal = () => {
+    //     // create dialog with Meal
+    // };
+
+    return (
+    <>
+        <FoodLayerProvider food={mealRef}>
+            <ConnectedUI mealRef={mealRef}>
+                <IngredientsList
+                    mealRef={mealRef}
+                />
+                <Stack
+                    isInline
+                    alignItems="center"
+                >
+                    <MealAdder />
+                    <FoodAdder />
+                </Stack>
+            </ConnectedUI>
+        </FoodLayerProvider>
+    </>
+    );
+};
+
 
 const ConnectedUI: React.FC<{ mealRef: Ref }> = ({
     mealRef,
@@ -57,5 +81,49 @@ const ConnectedUI: React.FC<{ mealRef: Ref }> = ({
         <ConnectedUI>
             {children}
         </ConnectedUI>
+    );
+};
+
+
+const MealAdder: React.FC = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const dispatch = useDispatch();
+    const onCreateMealClick = () => {
+        dispatch(setErrorMessage(`NOT IMPLEMENTED`));
+        // onOpen();
+        // TODO: dispatch action to create meal
+        // TODO: somehow, make that meal being written to "current dialog" in State
+    };
+
+    // TODO: create a ModalLayerPRovider in Onion
+    // TODO: in reducers, handle a "modal layer"
+    // TODO: in reducers, handle "set current day" scenario by having a "current day layer"
+
+    return (
+        <>
+            <Button
+                onClick={onCreateMealClick}
+                size="sm"
+                variant="outline"
+                variantColor="pink"
+            >
+                + Create and add new meal
+            </Button>
+            <Modal
+                isOpen={isOpen}
+                onClose={onClose}
+            >
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        Hello
+                        {/* <ModalLayerProvider>  */}
+                            {/* <Meal mealRef={} /> */}
+                        {/* </ModalLayerProvider> */}
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
     );
 };
