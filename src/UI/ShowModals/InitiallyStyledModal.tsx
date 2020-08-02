d1a7ee7b472f1035f455ec1d4d62734e0b5782ff
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2020-08-02 16:46:04 +0100

    Fix: modal closing logic
    The modal was erroring is clicked on its overlay
    or the Esc key was used.

--- src/UI/ShowModals/InitiallyStyledModal.tsx
+++ src/UI/ShowModals/InitiallyStyledModal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, useDisclosure, ModalOverlay, ModalContent, ModalCloseButton, ModalBody, Box } from "@chakra-ui/core";
+import { Modal, useDisclosure, ModalOverlay, ModalContent, ModalCloseButton, ModalBody, Box, ModalHeader } from "@chakra-ui/core";
 
 
 interface Props {
@@ -9,7 +9,7 @@ interface Props {
 export const InitiallyStyledModal: React.FC<Props> = ({ onModalClosing }) => {
     const { isOpen: isModalOpen, onClose: closeModal } = useDisclosure(true);
 
-    const onCloseButtonClick = () => {
+    const onUserActionToCloseModal = () => {
         closeModal();
         onModalClosing();
     };
@@ -17,10 +17,11 @@ export const InitiallyStyledModal: React.FC<Props> = ({ onModalClosing }) => {
     return (
         <Modal
             isOpen={isModalOpen}
+            onClose={onUserActionToCloseModal}
         >
             <ModalOverlay />
             <ModalContent>
-                <ModalCloseButton onClick={onCloseButtonClick} />
+                <ModalCloseButton onClick={onUserActionToCloseModal} />
                 <ModalBody>
                     Hello
                     <Box>
