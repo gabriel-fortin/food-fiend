87ec36d53ca649317157e8396fbd4229ccdac5d7
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2021-01-21 23:00:59 +0000

    Add triggering Week Editor from Week And Day Controls

--- src/Widget/WeekAndDayControls/Connector.tsx
+++ src/Widget/WeekAndDayControls/Connector.tsx
@@ -1,30 +1,59 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { useDisclosure } from "@chakra-ui/core";
 
 import { FoodType, Ingredient, Ref } from "Model";
 import { setCurrentDay, State, useAppState } from "Store";
 import { eqRef, filterOne } from "tools";
 
 import { WeeksAndDaysHorizontally as UI } from "./UI/WeeksAndDaysHorizontally";
+import { WeekEditor } from "Widget";
 
 
 export const Connector: React.FC = () => {
     const [selectedWeekRef, setSelectedWeekRef] = useState<Ref|null>(null);
+    const [editedWeekRef, setEditedWeekRef] = useState<Ref|null>(null);
     const [selectedDayOfWeek, setSelectedDayOfWeek] = useState<number|null>(null);
+    const {
+        isOpen: isWeekEditorOpen,
+        onOpen: openWeekEditor,
+        onClose: closeWeekEditor } = useDisclosure();
     const state = useAppState();
 
+    const onWeekEditorClose = (updatedWeekRef: Ref | null) => {
+        console.log(`Week And Day:: on Close: updatedWeekRef: `, updatedWeekRef);
+        if (updatedWeekRef !== null) {
+            setSelectedWeekRef(updatedWeekRef);
+        }
+        closeWeekEditor();
+    };
+
     const mapState = (state: State) => {
-        const weekData = state.getAllFoodOfType(FoodType.Week)
+        const weekData = state.getAllFoodOfType(FoodType.Week);
 
         return {
             weekData,
             selectedWeek: selectedWeekRef,
-            isPrevWeekAvailable: weekData !== [] && selectedWeekRef !== null && !eqRef(selectedWeekRef, weekData[0].ref),
-            isNextWeekAvailable: weekData !== [] && selectedWeekRef !== null && !eqRef(selectedWeekRef, weekData[weekData.length - 1].ref),
-        onPrevWeekSelected: () => console.warn(`NOT IMPLEMENTED: on prev week selected`),
-        onNextWeekSelected: () => console.warn(`NOT IMPLEMENTED: on next week selected`),
-        onWeekAdd: () => console.warn(`NOT IMPLEMENTED: on week add`),
-        onWeekEdit: () => console.warn(`NOT IMPLEMENTED: on week edit`),
+            isPrevWeekAvailable: weekData !== [] && selectedWeekRef !== null
+                && !eqRef(selectedWeekRef, weekData[0].ref),
+            isNextWeekAvailable: weekData !== [] && selectedWeekRef !== null
+                && !eqRef(selectedWeekRef, weekData[weekData.length - 1].ref),
+            onPrevWeekSelected: () => console.warn(`NOT IMPLEMENTED: on prev week selected`),
+            onNextWeekSelected: () => console.warn(`NOT IMPLEMENTED: on next week selected`),
+            onWeekAdd: () => {
+                // 'selectedWeekRef' is used to indicate which week is edited
+                // when adding a new week, it needs to be null
+                // setSelectedWeekRef(null);
+
+                setEditedWeekRef(null);
+
+                openWeekEditor();
+            },
+            onWeekEdit: () => {
+                setEditedWeekRef(selectedWeekRef);
+
+                openWeekEditor();
+            },
             selectedDay: selectedDayOfWeek,
             todayDay: null, // TODO
         };
@@ -39,7 +68,7 @@ export const Connector: React.FC = () => {
             return setCurrentDay(null);
         },
         onDaySelected: (dayOfWeek: number) => {
-            if (selectedWeekRef === null) return;
+            if (selectedWeekRef === null) return setCurrentDay(null);
 
             const positionMatchingDayOfWeek =
                 (ingredient: Ingredient) => ingredient.position === dayOfWeek;
@@ -53,6 +82,14 @@ export const Connector: React.FC = () => {
 
     const ConnectedUI = connect(mapState, mapDispatch)(UI);
     return (
-        <ConnectedUI/>
+        <>
+            <ConnectedUI/>
+            <WeekEditor
+                weekRef={editedWeekRef}
+                isOpen={isWeekEditorOpen}
+                onClose={onWeekEditorClose}
+            />
+        </>
     );
 };
+
